3dcf82d37cde06e8548adf99d915fef2e5343884
sickerman <henryoke158@yahoo.com>
2021-02-26 11:58:34 -0500

    icons

--- components/Hero.js
+++ components/Hero.js
@@ -16,28 +16,28 @@ function Hero() {
           height={120}
         />
       </div>
-      <h1 className="title">Web designer & developer</h1>
-
-      <p className="description">
-        Hello! my name is Henry Oke, I’m a self-taught web{" "}
-        <strong>developer</strong> and occasional designer currently based in
-        Toronto CA. My role as a developer is to help my clients build{" "}
-        <strong>websites</strong> and web-based <strong>applications</strong> to
-        meet business goals. Do you have a project in mind? Let’s work!
-      </p>
-
-      <div className="social-icons">
-        <div className="icon facebook">
-          <GrFacebookOption size={50} />
-        </div>
-        <div className="icon twitter">
-          <FaTwitter size={50} />
-        </div>
-        <div className="icon instagram">
-          <FaInstagram size={50} />
-        </div>
-        <div className="icon email">
-          <SiGmail size={50} />
+      <div className="text__content">
+        <h1 className="title">Web designer & developer</h1>
+        <p className="description">
+          Hello! my name is Henry Oke, I’m a self-taught web{" "}
+          <strong>developer</strong> and occasional designer currently based in
+          Toronto CA. My role as a developer is to help my clients build{" "}
+          <strong>websites</strong> and web-based <strong>applications</strong>{" "}
+          to meet business goals. Do you have a project in mind? Let’s work!
+        </p>
+        <div className="social-icons">
+          <div className="icon facebook">
+            <GrFacebookOption className="fb" size={50} />
+          </div>
+          <div className="icon twitter">
+            <FaTwitter className="fb" size={50} />
+          </div>
+          <div className="icon instagram">
+            <FaInstagram className="fb" size={50} />
+          </div>
+          <div className="icon email">
+            <SiGmail className="fb" size={50} />
+          </div>
         </div>
       </div>
       <div className="scroll_icon">
