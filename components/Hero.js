52f6f600f41634d7af386ae430be6687d6dff247
sickerman <henryoke158@yahoo.com>
2021-02-26 15:39:24 -0500

    hero complete

--- components/Hero.js
+++ components/Hero.js
@@ -19,13 +19,13 @@ function Hero() {
       <div className="text__content">
         <h1 className="title">Web designer & developer</h1>
         <p className="description">
-          Hello! my name is Henry Oke, I’m a self-taught web{" "}
+          Hello! my name is Henry Oke, I’m a Freelance web{" "}
           <strong>developer</strong> and occasional designer currently based in
           Toronto CA. My role as a developer is to help my clients build{" "}
           <strong>websites</strong> and web-based <strong>applications</strong>{" "}
           to meet business goals. Do you have a project in mind? Let’s work!
         </p>
-        <div className="social-icons">
+        {/* <div className="social-icons">
           <div className="icon facebook">
             <GrFacebookOption className="fb" size={50} />
           </div>
@@ -38,10 +38,10 @@ function Hero() {
           <div className="icon email">
             <SiGmail className="fb" size={50} />
           </div>
+        </div> */}
+        <div className="scroll_icon">
+          <span className="circle"></span>
         </div>
-      </div>
-      <div className="scroll_icon">
-        <span className="circle"></span>
       </div>
     </section>
   );
